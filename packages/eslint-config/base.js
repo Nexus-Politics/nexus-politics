e6d3cbaf869f3d3d4900e6299c88d62921d95738
Christoph Waffler <waffler.christoph@gmail.com>
2025-03-17 22:09:58 +0100

    chore: prettier sort and remove unused imports

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,8 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import unusedImports from "eslint-plugin-unused-imports";
+import importPlugin from "eslint-plugin-import";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +18,17 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      import: importPlugin,
+      "unused-imports": unusedImports,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "unused-imports/no-unused-imports": "warn",
+      "import/order": ["warn", {
+        "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+        "newlines-between": "always",
+        "alphabetize": { "order": "asc", "caseInsensitive": true }
+      }]
     },
   },
   {
